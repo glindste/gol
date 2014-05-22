ba70951933625ff766a3305f39f9d9236f65c562
Gustaf Lindstedt <gustaflindstedt@gmail.com>
2014-05-22 21:40:35 +0200

    broke out into class and made rows and cols dynamic

--- game_of_life.py
+++ game_of_life.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from drawille import Canvas
+from drawille import Canvas, getTerminalSize
 import time
 import curses
 import locale
@@ -9,112 +9,109 @@ import random
 locale.setlocale(locale.LC_ALL, "")
 
 stdscr = curses.initscr()
+stdscr.nodelay(True)
 stdscr.refresh()
 
-rows = 100
-columns = 100
+class GameOfLife:
 
-def neighbour_count(state, x, y):
-    nburs = 0
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.can = Canvas()
+        self.state = [[bool(random.getrandbits(1)) for y in range(rows)] for x in range(cols)]
 
-    # Left
-    if x > 0:
-        if state[x-1][y]:
-            nburs += 1
-        # Top
-        if y > 0:
-            if state[x-1][y-1]:
-                nburs += 1
-        # Bottom
-        if y < rows-1:
-            if state[x-1][y+1]:
-                nburs += 1
+    def tick(self):
+        next_gen = [[False for y in range(self.rows)] for x in range(self.cols)]
 
-    # Right
-    if x < columns-1:
-        if state[x+1][y]:
-            nburs += 1
-        # Top
-        if y > 0:
-            if state[x+1][y-1]:
-                nburs += 1
-        # Bottom
-        if y < rows-1:
-            if state[x+1][y+1]:
-                nburs += 1
-
-    # Top
-    if y > 0:
-        if state[x][y-1]:
-            nburs += 1
-    # Bottom
-    if y < rows-1:
-        if state[x][y+1]:
-            nburs += 1
-
-    return nburs
-
-
-def __main__(stdsrc):
-
-    state = [[bool(random.getrandbits(1)) for y in range(rows)] for x in range(columns)]
-
-    c = Canvas()
-
-    f = c.frame(0,0,columns,rows)
-    stdscr.addstr(0, 0, '{0}\n'.format(f))
-    stdscr.refresh()
-
-    while True:
-
-        next_gen = [[False for y in range(rows)] for x in range(columns)]
-
-        for y in range(rows):
-            for x in range(columns):
-                nburs = neighbour_count(state, x, y)
-                if state[x][y]:
+        for y in range(self.rows):
+            for x in range(self.cols):
+                nburs = self._ncount(x, y)
+                if self.state[x][y]:
                     # Alive
                     if nburs > 1 and nburs < 4:
-                        c.set(x,y)
+                        self.can.set(x,y)
                         next_gen[x][y] = True
                     else:
                         next_gen[x][y] = False
                 else:
                     # Dead
                     if nburs == 3:
-                        c.set(x,y)
+                        self.can.set(x,y)
                         next_gen[x][y] = True
                     else:
                         next_gen[x][y] = False
 
-        f = c.frame(0,0,columns,rows)
+        self.state = next_gen
+
+    def draw(self):
+        f = self.can.frame(0,0,self.cols,self.rows)
         stdscr.addstr(0, 0, '{0}\n'.format(f))
         stdscr.refresh()
-
-        c.clear()
-
-        state = next_gen
-        #time.sleep(0.3)
+        self.can.clear()
 
 
-def printState(state):
-        s = ' '
-        for i in range(len(state[0])):
-              s += ' '+str(i)
-        for i, row in enumerate(state):
-              s += '\n\r'
-              s += str(i)
-              for col in state[i]:
-                  s += ' '
-                  if col:
-                      s+='A'
-                  else:
-                      s+='D'
+    def _ncount(self, x, y):
+        nburs = 0
 
-        s+='\n\r'
+        # Left
+        if x > 0:
+            if self.state[x-1][y]:
+                nburs += 1
+            # Top
+            if y > 0:
+                if self.state[x-1][y-1]:
+                    nburs += 1
+            # Bottom
+            if y < self.rows-1:
+                if self.state[x-1][y+1]:
+                    nburs += 1
 
-        print(s)
+        # Right
+        if x < self.cols-1:
+            if self.state[x+1][y]:
+                nburs += 1
+            # Top
+            if y > 0:
+                if self.state[x+1][y-1]:
+                    nburs += 1
+            # Bottom
+            if y < self.rows-1:
+                if self.state[x+1][y+1]:
+                    nburs += 1
 
+        # Top
+        if y > 0:
+            if self.state[x][y-1]:
+                nburs += 1
+        # Bottom
+        if y < self.rows-1:
+            if self.state[x][y+1]:
+                nburs += 1
+
+        return nburs
+
+def __main__(stdsrc):
+
+    # Get terminal size
+    term_x, term_y = getTerminalSize()
+
+    # Set rows and columns
+    columns = (term_x-1)*2
+    rows = (term_y-1)*4
+
+    g = GameOfLife(rows,columns)
+
+    g.draw()
+
+    while True:
+
+        g.tick()
+        g.draw()
+
+        # Catch user input
+        inp = stdscr.getch()
+        if inp != curses.ERR:
+            break
 
 if __name__ == "__main__":
     curses.wrapper(__main__)
